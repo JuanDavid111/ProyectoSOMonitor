29a242270db63a880bef7bb9343d2ee6aaec6af5
Diego Heredia <diego.heredia@grupoconsultic.com.mx>
2025-06-22 00:03:01 -0400

    cambios a leerinit.c

--- leerini.c
+++ leerini.c
@@ -3,11 +3,18 @@
 #include<unistd.h>
 #include<sys/stat.h>
 #include<errno.h>
+#include<string.h>
+#include<ctype.h>
 #include<syslog.h>
 
 #define ARCHIVO_CONFIG "/etc/proyecto so 1/proy1.ini"
 #define LOG_DIR "/var/log/PROYECTO SO 1"
 #define CONFIG_DIR "/etc/proyecto so 1"
+
+typedef struct{
+    char log_tag[128];
+    int interval;
+}Appconfig;
 
 
 int crear_directorio_config(){
@@ -39,7 +46,60 @@ int verificar_archivo_ini(){
     }
 }
 
+char* limpiar_linea(char *linea){
+    char* fin_linea;
+
+    while(isspace((unsigned char)*linea)) linea++;
+    fin_linea = linea + strlen(linea) - 1;
+    while(fin_linea > linea && isspace((unsigned char)*fin_linea)) fin_linea--;
+
+    *(fin_linea+1) = 0;
+
+    return linea;
+}
+
+int obtener_info_config(Appconfig *config){
+    FILE *archivoINI = fopen(ARCHIVO_CONFIG, "r");
+
+    char linea[256];
+    int marcador_conf = 0;
+
+    while (fgets(linea, sizeof(linea), archivoINI) != NULL) {
+        char *linea_limpia = limpiar_linea(linea);
+
+        if (strlen(linea_limpia) == 0 || linea_limpia[0] == ';') { // Salto de lineas en blanco y ; (comentarios)
+            continue;
+        }
+
+        if (strcmp(linea_limpia, "[CONF]") == 0) { // Lee [CONF] y lo salta
+            marcador_conf = 1;
+            continue;
+        }
+
+        if (marcador_conf) { // Dentro de [CONF]
+            char *key = linea_limpia;
+            char *value = strchr(linea_limpia, '='); // Busca el signo =
+
+            if (value != NULL) { //quita el =
+                *value = '\0';
+                value++;
+
+                if (strcmp(key, "log_tag") == 0) {
+                    strncpy(config->log_tag, value, sizeof(config->log_tag) - 1);
+                    config->log_tag[sizeof(config->log_tag) - 1] = '\0';
+                } else if (strcmp(key, "interval") == 0) {
+                    config->interval = atoi(value);
+                }
+            }
+        }
+    }
+
+    fclose(archivoINI);
+    return 0;
+}
+
 int main(){
+    Appconfig config;
     openlog("PROYECTO_SO1_DAEMON", LOG_PID | LOG_CONS, LOG_DAEMON);
 
     crear_directorio_log();
@@ -50,7 +110,7 @@ int main(){
         return 0;
     }
 
-    syslog(LOG_INFO, "Aplicación daemon iniciada y procesando...");
+    obtener_info_config(&config);
 
     closelog();
 }
